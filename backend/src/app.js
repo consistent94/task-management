fadbaa879f66838e1bd2469d2d2f620af8be9b87
Seb <sebwilczega@gmail.com>
2025-01-03 14:19:26 +0000

    db successful

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 // Routes
-app.use('/auth', authRoutes);
+app.use('/api/auth', authRoutes);
 
 //Error handling middleware
 app.use((err, req, res, next) => {
